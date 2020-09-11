eb91fc971458d6ecdac04b643f59cef669a4d903
Isaque Araújo Nogueira <isaquean2@gmail.com>
2020-09-11 16:44:11 -0300

    Projeto iniciado,subtraindo duas datas e printando

---
+++ POO2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace POO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime t1 = new DateTime(2020, 09, 02);
+            DateTime t2 = new DateTime(2020, 09, 05);
+            TimeSpan s1 = t2.Subtract(t1);
+            Console.WriteLine(s1);
+
+
+        }
+    }
+}
